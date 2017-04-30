66225a315816fc615fd66936fb6f6c6fec7553d0
cindy_zh <cindy.zhangheng@gmail.com>
2017-04-30 19:53:59 -0400

    with trick log document

--- test.py
+++ test.py
@@ -218,8 +218,8 @@ model.add(Dropout(0.25))
 # model.add(Conv2D(FEATURES_2, 2,2, activation='relu'))
 # model.add(MaxPooling2D(pool_size=(2, 2)))
 
-#model.add(Conv2D(64, 3, 3, activation='relu'))
-#model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, 3, 3, activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
 model.add(Dense(128))
